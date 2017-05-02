bfaf2e38fa5f982d20a16501869219c4b029a9ae
eih5176 <eih5176@BKGAIGE209-101.win.psu.edu>
2017-05-02 13:08:48 -0400

    More character models

---
+++ TurnBasedStrategy/src/turnbasedstrategy/ClassAnimations.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turnbasedstrategy;
+
+/**
+ *
+ * @author eih5176
+ */
+public class ClassAnimations {
+    
+   String aType, cType, pos;
+    
+    public ClassAnimations()
+    {
+        
+    }
+    
+    public void playAnimation(String a, String c, String p)
+    {
+        aType = a;
+        cType = c;
+        pos = p;
+        
+        if (cType == "Knight"){
+            
+        }
+    }
+    
+    
+}
